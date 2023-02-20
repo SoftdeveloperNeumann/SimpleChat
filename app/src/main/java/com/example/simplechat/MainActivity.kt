b46ece7d783c1e68eb8b05c3a237336e267cc18b
Frank <f.neumann.berlin@googlemail.com>
2023-02-20 08:47:53 +0100

    Fertige Main Activity

--- app/src/main/java/com/example/simplechat/MainActivity.kt
+++ app/src/main/java/com/example/simplechat/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.simplechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.simplechat.databinding.ActivityMainBinding
@@ -12,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnToChat.setOnClickListener {view ->
+            val intent = Intent(this,ChatAktivity::class.java).apply {
+                putExtra("user", binding.etUser.text.toString())
+                putExtra("other",binding.etOther.text.toString())
+            }
+            startActivity(intent)
+        }
     }
 }
